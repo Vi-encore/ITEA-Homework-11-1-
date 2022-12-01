aacd46a496e01cef30df932aede7019cc1aa3bb9
Vi <vikamatuchko772@gmail.com>
2022-12-01 09:52:12 +0200

    first commit

---
+++ js/task3.js
@@ -0,0 +1,14 @@
+//3. Оголосіть дві змінні: admin та name.
+//Запишіть рядок "Джон" у змінну назву.
+//Скопіюйте значення зі змінної name в admin.
+//Виведіть на екран значення admin, використовуючи функцію console.log (має показати «Джон»).
+
+function adminName() {
+  let name;
+  let admin;
+  name = "Джон";
+  admin = name;
+  console.log(admin);
+}
+
+adminName();
